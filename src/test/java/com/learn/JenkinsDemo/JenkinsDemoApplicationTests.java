e0e736ffebecb566a5f6a6dfe8f55482875a5c38
Deepak Yadav <deepakyadav1331@gmail.com>
2021-07-23 03:10:26 +0530

    Added postConstruct and test case

--- src/test/java/com/learn/JenkinsDemo/JenkinsDemoApplicationTests.java
+++ src/test/java/com/learn/JenkinsDemo/JenkinsDemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.learn.JenkinsDemo;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinsDemoApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(JenkinsDemoApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("Executing test case...");
+		assertEquals(true, true);
 	}
 
 }
